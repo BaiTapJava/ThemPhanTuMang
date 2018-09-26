9e0a15167916eeaf0f66cb5e29ed7c0c9656dbf4
ThinhLuffy <41873503+ThinhLuffy@users.noreply.github.com>
2018-09-26 21:11:23 +0700

    ChenMang

---
+++ ChenMang/src/ChenMang.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class ChenMang {
+    Scanner scanner = new Scanner(System.in);
+    int value, index, n;
+    int[] array = new int[100];
+
+    public static void main(String[] args) {
+        ChenMang insertValue = new ChenMang();
+        Input(insertValue);
+        Output(insertValue);
+    }
+
+    public static void Input(ChenMang insertValue) {
+        System.out.print("Nhập vào số lượng phần tử của mảng: ");
+        insertValue.n = insertValue.scanner.nextInt();
+        insertValue.array = new int[insertValue.n + 1];
+        insertValue.array[insertValue.n] = 0;
+
+        System.out.println("Nhập các giá các phẩn tử của mảng: ");
+        for (int i = 0; i < insertValue.n; i++) {
+            System.out.print("array[" + i + "]= ");
+            insertValue.array[i] = insertValue.scanner.nextInt();
+        }
+
+        System.out.print("Nhập vào phần tử cần chèn: ");
+        insertValue.value = insertValue.scanner.nextInt();
+
+        System.out.print("Nhập vào vị trí chèn: ");
+        insertValue.index = insertValue.scanner.nextInt();
+
+        System.out.println("Các phẩn tử mảng trước khi chèn là:");
+        for (int i = 0; i < insertValue.array.length - 1; i++) {
+            System.out.printf("%5d", insertValue.array[i]);
+        }
+    }
+
+    public static void Output(ChenMang insertValue) {
+        if (insertValue.index > (insertValue.array.length) || insertValue.index < 0) {
+            System.out.print("Vị trí chèn vướt giới hạn!");
+        } else {
+            for (int i = insertValue.array.length - 1; i >= insertValue.index - 1; i--) {
+                if (i == insertValue.index - 1) {
+                    insertValue.array[i] = insertValue.value;
+                } else {
+                    insertValue.array[i] = insertValue.array[i - 1];
+                }
+            }
+            System.out.println("\nDanh sách phẩn tử sau khi chèn vào là: ");
+
+            for (int i = 0; i < insertValue.array.length; i++) {
+                System.out.printf("%5d", insertValue.array[i]);
+            }
+        }
+    }
+}
